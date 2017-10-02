0b9506e91f9a4c94a8dd852ec0425c7d17997016
sunloving <ilfirin.nar@gmail.com>
2017-10-02 06:39:39 +0300

    patricia trie tests

--- src/tests/Jbta.SearchEngine.IntegrationTests/PatriciaTrieTests.cs
+++ src/tests/Jbta.SearchEngine.IntegrationTests/PatriciaTrieTests.cs
@@ -9,17 +9,76 @@ namespace Jbta.SearchEngine.IntegrationTests
     public class PatriciaTrieTests
     {
         [Fact]
-        public void SimpleWordsWithNumbers_JustAdded_SuccessedRetrieved()
+        public void Get_SimpleWordsWithNumbers_SuccessedRetrieved()
+        {
+            var (trie, map) = GetTrieWithData();
+
+            foreach (var kv in map)
+            {
+                Assert.Equal(kv.Value, trie.Get(kv.Key).First());
+            }
+        }
+
+        [Fact]
+        public void Get_NotWholeWord_SuccessedRetrieved()
+        {
+            var (trie, map) = GetTrieWithData();
+            var result = trie.Get("арбуз");
+            Assert.Equal(map["арбуз"], result.First());
+        }
+
+        [Fact]
+        public void Get_NotWholeWord_CorrectResultsNumber()
+        {
+            var (trie, _) = GetTrieWithData();
+            var result = trie.Get("арбузка");
+            Assert.Equal(1, result.Count());
+        }
+
+        [Fact]
+        public void Get_NotWholeWordPartOfExistingKey_ZeroResults()
+        {
+            var (trie, _) = GetTrieWithData();
+            var result = trie.Get("арбу");
+            Assert.Equal(4, result.Count());
+        }
+
+        [Fact]
+        public void Get_WholeWord_SuccessedRetrieved()
+        {
+            var (trie, map) = GetTrieWithData();
+            var result = trie.Get("арбуз", true);
+            Assert.Equal(map["арбуз"], result.First());
+        }
+
+        [Fact]
+        public void Get_WholeWord_CorrectResultsNumber()
+        {
+            var (trie, _) = GetTrieWithData();
+            var result = trie.Get("арбуз", true);
+            Assert.Equal(1, result.Count());
+        }
+
+        [Fact]
+        public void Get_WholeWordPartOfExistingKey_ZeroResults()
+        {
+            var (trie, _) = GetTrieWithData();
+            var result = trie.Get("арбу", true);
+            Assert.Equal(0, result.Count());
+        }
+
+        private static (ITrie<int> trie, IDictionary<string, int> map) GetTrieWithData()
         {
             var randomizer = new Random();
             var words = new Dictionary<string, int>
             {
-                { "арбуз", randomizer.Next() },
-                { "аркебуза", randomizer.Next() },
-                { "арбузяка", randomizer.Next() },
-                { "арбузка", randomizer.Next() },
-                { "арбузяшка", randomizer.Next() },
-                { "арлекин", randomizer.Next() }
+                {"арбуз", randomizer.Next()},
+                {"аркебуза", randomizer.Next()},
+                {"арбузяка", randomizer.Next()},
+                {"арбузка", randomizer.Next()},
+                {"арбузяшка", randomizer.Next()},
+                {"арлекин", randomizer.Next()},
+                {"аптека", randomizer.Next()},
             };
             var trie = new PatriciaTrie<int>();
 
@@ -28,10 +87,7 @@ namespace Jbta.SearchEngine.IntegrationTests
                 trie.Add(kv.Key, kv.Value);
             }
 
-            foreach (var kv in words)
-            {
-                Assert.Equal(kv.Value, trie.Get(kv.Key).First());
-            }
+            return (trie, words);
         }
     }
 }
