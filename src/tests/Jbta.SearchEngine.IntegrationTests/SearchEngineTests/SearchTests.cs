f61f2c09bd8de2e11c0ba4a1f17fbc6c65acec62
sunloving <ilfirin.nar@gmail.com>
2017-10-06 01:53:42 +0300

    another couple of search tests

--- src/tests/Jbta.SearchEngine.IntegrationTests/SearchEngineTests/SearchTests.cs
+++ src/tests/Jbta.SearchEngine.IntegrationTests/SearchEngineTests/SearchTests.cs
@@ -39,6 +39,53 @@ namespace Jbta.SearchEngine.IntegrationTests.SearchEngineTests
             });
         }
 
+        [Theory]
+        [InlineData("Кутузов", 0)]
+        [InlineData("Кутузова", 2)]
+        [InlineData("князь", 40)]
+        [InlineData("Наташа", 0)]
+        [InlineData("что", 101)]
+        public void Search_WholeWord_ExpectedEntriesCount(string query, int cout)
+        {
+            IndexFileAndDoAction(TestTextFiles.WarAndPeace1, (engine, filePath) =>
+            {
+                var wordEntries = engine.Search(query, true).ToList();
+
+                Assert.Equal(cout, wordEntries.Count);
+            });
+        }
+
+        [Theory]
+        [InlineData("Кутузов", 1)]
+        [InlineData("князь", 1)]
+        [InlineData("Наташа", 0)]
+        [InlineData("что", 1)]
+        public void SearchFiles_ByPrefix_ExpectedFilesCount(string query, int cout)
+        {
+            IndexFileAndDoAction(TestTextFiles.WarAndPeace1, (engine, filePath) =>
+            {
+                var wordEntries = engine.SearchFiles(query).ToList();
+
+                Assert.Equal(cout, wordEntries.Count);
+            });
+        }
+
+        [Theory]
+        [InlineData("Кутузов", 0)]
+        [InlineData("Кутузова", 1)]
+        [InlineData("князь", 1)]
+        [InlineData("Наташа", 0)]
+        [InlineData("что", 1)]
+        public void SearchFiles_WholeWord_ExpectedFilesCount(string query, int cout)
+        {
+            IndexFileAndDoAction(TestTextFiles.WarAndPeace1, (engine, filePath) =>
+            {
+                var wordEntries = engine.SearchFiles(query, true).ToList();
+
+                Assert.Equal(cout, wordEntries.Count);
+            });
+        }
+
         private static async void IndexFileAndDoAction(string filePath, Action<ISearchEngine, string> action)
         {
             var engine = SearchEngineFactory.New();
