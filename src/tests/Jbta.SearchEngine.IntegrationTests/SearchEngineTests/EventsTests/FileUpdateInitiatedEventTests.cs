50ca73b4807359dda4eee38aaa6c7d71d84f7a10
sunloving <ilfirin.nar@gmail.com>
2017-10-16 19:03:48 +0300

    more tests

--- src/tests/Jbta.SearchEngine.IntegrationTests/SearchEngineTests/EventsTests/FileUpdateInitiatedEventTests.cs
+++ src/tests/Jbta.SearchEngine.IntegrationTests/SearchEngineTests/EventsTests/FileUpdateInitiatedEventTests.cs
@@ -30,5 +30,33 @@ namespace Jbta.SearchEngine.IntegrationTests.SearchEngineTests.EventsTests
                 Assert.True(await tcs.Task);
             }
         }
+
+        [Fact]
+        public async void FileUpdateInitiated_FileInFolderChanged_Raised()
+        {
+            var engine = SearchEngineFactory.New();
+            var tcs = new TaskCompletionSource<bool>();
+
+            using (var folder = new TestFolder())
+            {
+                var folderPath = folder.Path;
+                using (var file = new TestFile("foo", folderPath))
+                {
+                    var filePath = file.Path;
+
+                    engine.Add(folderPath);
+                    await Task.Delay(100);
+
+                    engine.FileUpdateInitiated += args =>
+                    {
+                        tcs.TrySetResult(Path.GetFullPath(filePath) == args.Path);
+                    };
+
+                    file.ChangeContent("bar");
+
+                    Assert.True(await tcs.Task);
+                }
+            }
+        }
     }
 }
